c2913767bbae4a915335116f9451e0fbcfd00cd8
Israr <israr.akh03@gmail.com>
2023-05-19 19:21:57 -0400

    Just added my TestClass for Encapsulation

---
+++ src/practice_java/TestsClass.java
@@ -0,0 +1,13 @@
+package practice_java;
+
+public class TestsClass {
+
+	public static void main(String[] args) {
+
+		EncapsulationPractice obj = new EncapsulationPractice();
+		obj.setName("Khalid Hadizada");
+		obj.setPhoneNumber("0730303030");
+		System.out.println(obj.getName());
+		System.out.println(obj.getPhoneNumber());
+	}
+}
